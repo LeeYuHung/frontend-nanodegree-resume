04da829c0e5c3f8c28ccc6d44cc414af9077bdc7
Hung <rossa023969@gmail.com>
2016-07-04 01:11:22 +0800

    Finish Quiz: Internationalize Names

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -73,8 +73,40 @@ var education = {
   ]
 }
 
+
+$("#header").append(HTMLheaderName.replace("%data%", bio.name));
 if(bio["skills"]) {
   $("#header").append(HTMLskillsStart);
-  for(skill in bio.skills)
-    $("#skills").append(HTMLskills.replace("%data%", skill));
+  for(idx in bio.skills){
+    $("#skills").append(HTMLskills.replace("%data%", bio.skills[idx]));
+  }
 }
+
+function displayWork() {
+  if(work["jobs"]) {
+    work.jobs.forEach(function(job, idx) {
+      var $work_entry;
+      $("#workExperience").append(HTMLworkStart);
+      $work_entry = $(".work-entry").eq(idx);
+      $work_entry.append(HTMLworkEmployer.replace("%data%", job.employer) + HTMLworkTitle.replace("%data%", job.title));
+      $work_entry.append(HTMLworkDates.replace("%data%", job.dates));
+      $work_entry.append(HTMLworkDescription.replace("%data%", job.description));
+    });  
+  }
+}
+displayWork();
+
+$("#main").append(internationalizeButton);
+
+function inName(full_name) {
+  var first_name= full_name.split(" ")[0];
+  var last_name = full_name.split(" ")[1];
+  var international_name;
+  first_name = first_name.charAt(0).toUpperCase() + first_name.slice(1).toLowerCase();
+  last_name = last_name.toUpperCase();
+  international_name = first_name + " " + last_name;
+
+  return international_name;
+}
+
+console.log(inName("Huey Lee"));
